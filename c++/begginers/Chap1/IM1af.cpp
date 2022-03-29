01f46817141a760312d224658006b9a254091ad0
Archita Roy Choudhury <67641687+archita2k1@users.noreply.github.com>
2022-03-29 18:47:12 +0530

    Delete c++/begginers directory

--- c++/begginers/Chap1/IM1af.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: \\U1Chap01\IM1af.CPP
-#include<iostream.h>
-#include<conio.h>
-void main()
-{
-	for(int Lvalue = 9; Lvalue >= 9; Lvalue - 1)
-		cout << Lvalue << endl;
-	cout << Lvalue + 1 << endl << Lvalue - 1 << endl;
-}
--- c++/begginers/Chap1/IM1ag.cpp
+++
@@ -1,9 +0,0 @@
-// Filename: \\U1Chap01\IM1ag.CPP
-#include<iostream.h>
-#include<conio.h>
-int a = 10;
-main()
-{
-	int a = 5;
-	cout << a << ::a;;
-}
